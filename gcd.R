10a323c05eb53b2626284833d814cccb2017086a
skarmanegra-rgb <skarmanegra@gmail.com>
2026-02-05 15:48:45 +0530

    Add GCD function implementation in gcd.R

---
+++ gcd.R
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d", gcd(x, y));
+    return 0;
+}
